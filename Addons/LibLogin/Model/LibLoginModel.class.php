07e3cc60451e88ae4768252b4aebc7f76cd19381
Scarecrow1024 <819681825@qq.com>
2016-09-12 17:35:43 +0800

    124

---
+++ Addons/LibLogin/Model/LibLoginModel.class.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Addons\LibLogin\Model;
+use Home\Model;
+
+/**
+ * LibLogin模型
+ */
+class LibLoginModel extends Model{
+    
+}
---
+++ Addons/RepairLogin/Model/RepairLoginModel.class.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Addons\Repair\Model;
+use Home\Model;
+
+/**
+ * Repair模型
+ */
+class RepairModel extends Model{
+
+}
