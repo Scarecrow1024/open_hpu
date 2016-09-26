899ce7fd350565e5666747e5d6d377dc979dd49e
Scarecrow1024 <819681825@qq.com>
2016-09-26 20:52:44 +0800

    重新修改课表

--- Addons/Binding/Controller/BindingController.class.php
+++ Addons/Binding/Controller/BindingController.class.php
@@ -337,6 +337,9 @@ class BindingController extends AddonsController{
                     if(count($arr[$i+1])==7&&count($arr[$i+2])==7){
                         $data[$i][]=$arr[$i+2];
                     }
+                    if(count($arr[$i+1])==7&&count($arr[$i+3])==7){
+                        $data[$i][]=$arr[$i+3];
+                    }
                 }   
             }
         }
@@ -470,6 +473,9 @@ class BindingController extends AddonsController{
                     }
                     if(count($arr[$i+1])==7&&count($arr[$i+2])==7){
                         $data[$i][]=$arr[$i+2];
+                    }
+                    if(count($arr[$i+1])==7&&count($arr[$i+3])==7){
+                        $data[$i][]=$arr[$i+3];
                     }
                 }   
             }
